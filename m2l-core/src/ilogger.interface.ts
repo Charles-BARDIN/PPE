d253909a017849270f53de7739bc20508ed3a823
Charles Bardin <c.bardin@aceforschool.com>
2017-02-06 16:08:57 +0100

    mode ILogger declaration to m2l-core

--- m2l-core/src/ilogger.interface.ts
+++ m2l-core/src/ilogger.interface.ts
@@ -1,9 +1,11 @@
-interface ILogger {
+export interface ILogger {
   debug(...args: any[]): void;
+
   log(...args: any[]): void;
+
   info(...args: any[]): void;
+
   warn(...args: any[]): void;
+  
   error(...args: any[]): void;
 }
-
-export { ILogger };
