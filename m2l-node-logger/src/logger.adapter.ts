10052964dea9ff2c2c3fefe6ad2a30733b47a8a6
Charles BARDIN <charles.bardin.pro@gmail.com>
2017-03-21 16:48:10 +0100

    bug fix in logger

--- m2l-node-logger/src/logger.adapter.ts
+++ m2l-node-logger/src/logger.adapter.ts
@@ -4,23 +4,23 @@ class LoggerAdapter implements ILogger {
   constructor() { }
 
   public debug(...args: any[]) {
-    console.log.call(console, arguments);
+    console.log.call(console, ...args);
   }
 
   public log(...args: any[]) {
-    console.log.call(console, arguments);
+    console.log.call(console, ...args);
   }
 
   public info(...args: any[]) {
-    console.log.call(console, arguments);
+    console.log.call(console, ...args);
   }
 
   public warn(...args: any[]) {
-    console.log.call(console, arguments);
+    console.log.call(console, ...args);
   }
 
   public error(...args: any[]) {
-    console.log.call(console, arguments);
+    console.log.call(console, ...args);
   }
 }
 
