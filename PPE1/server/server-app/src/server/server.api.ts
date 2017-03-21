977aef4f02f61bf00b616af61128bc444b72dbc9
Charles BARDIN <charles.bardin.pro@gmail.com>
2017-03-21 17:06:11 +0100

    add server start

--- PPE1/server/server-app/src/server/server.api.ts
+++ PPE1/server/server-app/src/server/server.api.ts
@@ -11,16 +11,23 @@ export class Server {
   private _room: RoomAdapter;
   private _user: UserAdapter;
 
+  private _logger: ILogger;
+
   private _server: express.Application;
 
   constructor(config: { logger: ILogger }) {
     this._booking = new BookingAdapter({ logger: config.logger });
     this._room = new RoomAdapter({ logger: config.logger });
     this._user = new UserAdapter({ logger: config.logger });
+
+    this._logger = config.logger;
+
     this._server = express();
   }
 
   public start() {
-
+    this._server.listen(9090, () => {
+      this._logger.log('Listening on 9090...');
+    })
   }
 }
