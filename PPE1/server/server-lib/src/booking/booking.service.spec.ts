2c13c2fbafffa56d838c6eea39d133b79bd966c6
metaxiii <lehchibi.gael@gmail.com>
2017-03-29 17:11:17 +0200

    Add test unit

--- PPE1/server/server-lib/src/booking/booking.service.spec.ts
+++ PPE1/server/server-lib/src/booking/booking.service.spec.ts
@@ -12,17 +12,17 @@ describe('Booking Service', () => {
         dataAccess = {
             add: function add(booking: Booking): Promise<Booking> {
                 return new Promise((resolve, reject) => {
-
+                    resolve({ roomID: 1, userID: 1, date: new Date() });
                 });
             },
             get: function get(booking: { roomID: number, date?: Date }): Promise<Booking[]> {
                 return new Promise((resolve, reject) => {
-
+                    resolve([]);
                 });
             },
             remove: function remove(booking: Booking): Promise<boolean> {
                 return new Promise((resolve, reject) => {
-
+                    resolve(true);
                 });
             }
         };
@@ -38,9 +38,62 @@ describe('Booking Service', () => {
 
     describe('bookARoom', () => {
         it('Should return a Promise', () => {
-            let result = bookingService.bookARoom({roomID: 1, userID: 1, date: new Date()});
+            let book = { roomID: 1, userID: 1, date: new Date() };
+
+            let result = bookingService.bookARoom(book);
 
             expect(result).to.be.an.instanceof(Promise);
+        });
+
+        it('Should call the get method of the dataAccess', done => {
+            dataAccess.get = function get(booking: Booking): Promise<Booking[]> {
+                return new Promise((resolve, reject) => {
+                    done();
+                });
+            };
+            let book = { roomID: 1, userID: 1, date: new Date() };
+
+            let result = bookingService.bookARoom(book);
+        });
+
+        it('Should call the add method of the dataAccess', done => {
+            dataAccess.add = function add(booking: Booking): Promise<Booking> {
+                return new Promise((resolve, reject) => {
+                    done();
+                });
+            };
+            let book = { roomID: 1, userID: 1, date: new Date() };
+
+            let result = bookingService.bookARoom(book);
+        });
+
+        it('Should reject the promise if the room is taken', (done) => {
+            dataAccess.get = function get(booking: { roomID: number, date: Date }): Promise<Booking[]> {
+                return new Promise((resolve, reject) => {
+                    resolve([
+                        {
+                            roomID: booking.roomID,
+                            date: booking.date,
+                            userID: 9
+                        }
+                    ]);
+                });
+            };
+            let book = { roomID: 1, userID: 1, date: new Date() };
+
+            let result = bookingService.bookARoom(book)
+                .catch(() => {
+                    done();
+                });
+        });
+
+        it('Should resolve the promise if the room is not taken', (done) => {
+            let book = { roomID: 1, userID: 1, date: new Date() };
+
+            let result = bookingService.bookARoom(book)
+                .then(() => {
+                    done();
+                });
         });
     });
 });
