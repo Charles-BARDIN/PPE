b926b06a9b06d5df83fb0d079ff00673cde4667c
metaxiii <lehchibi.gael@gmail.com>
2017-04-01 17:41:21 +0200

    Add VueJS

--- PPE1/booking-client/booking-client-app/src/router/index.js
+++ PPE1/booking-client/booking-client-app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Header from '@/components/Header'
 
 Vue.use(Router)
 
@@ -10,6 +11,12 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path : '/index',
+      name : 'Header' , 
+      component : Header 
     }
+
   ]
 })
