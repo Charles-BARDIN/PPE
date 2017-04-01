f4642cfab397c3bad6dad3f5b8f2c2e3f0e9b42d
Charles Bardin <c.bardin@aceforschool.com>
2017-04-01 23:35:56 +0200

    create Salle component

--- PPE1/booking-client/booking-client-app/src/components/Salle.vue
+++ PPE1/booking-client/booking-client-app/src/components/Salle.vue
@@ -1,21 +1,50 @@
 <template>
-    <div class="jumbotron notopnobottom">
+    <div class="salle">
       <h1>Liste des salles</h1>
-      <div class="flexcenter">
-        <form action="">
-          <select name="room" id="room">
-            <option value="amphiteatre">amphiteatre</option>
-            <option value="reunion">Salle de réunion</option>
-            <option value="convivial">Salle de convivialité</option>
-          </select>
-        </form>
+
+      <form action="">
+        <select name="room" id="room" v-model="selectedRoomLabel">
+          <option v-for="room in rooms">{{ room.label }}</option>
+        </select>
+      </form>
+
+      <div class="room-description">
+        {{ selectedRoom.description }}
       </div>
     </div>
 </template>
 
 <script>
   export default {
-    name: 'salle'
+    name: 'salle',
+    data () {
+      return {
+        rooms: [
+          {
+            label: 'Amphithéâtre',
+            img: '',
+            description: 'Description Amphithéâtre'
+          },
+          {
+            label: 'Salle de réunion',
+            img: '',
+            description: 'Description Salle de réunion'
+          },
+          {
+            label: 'Salle de convivialité',
+            img: '',
+            description: 'Description Salle de convivialité'
+          }
+        ],
+        selectedRoom: {},
+        selectedRoomLabel: ''
+      }
+    },
+    watch: {
+      selectedRoomLabel: function(newRoom) {
+        this.selectedRoom = this.rooms.filter(room => room.label === newRoom)[0]
+      }
+    }
   }
 </script>
 
@@ -23,5 +52,10 @@
 <style scoped>
   h1 {
     font-weight: normal;
+  }
+
+  .room-description {
+    text-align: justify;
+    margin-top: 20px;
   }
 </style>
