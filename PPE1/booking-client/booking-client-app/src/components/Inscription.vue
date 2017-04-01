ca9807e9b0ad48605f4a0386e6e748bbd32b1bd7
Charles Bardin <c.bardin@aceforschool.com>
2017-04-01 22:31:24 +0200

    organize form

--- PPE1/booking-client/booking-client-app/src/components/Inscription.vue
+++ PPE1/booking-client/booking-client-app/src/components/Inscription.vue
@@ -1,67 +1,97 @@
 <template>
     <div class="jumbotron notopnobottom">
       <h1>Inscription</h1>
-      <form action="">
-        <label for="lastname" class="labelo">Nom </label>:<input type="text" name="lastname" class="space" /> <br/>
-        <label for="firstname" class="labelo">Prénom</label>:<input type="text" name="firstname" class="space" /> <br/>
-        <label for="numberstreet" class="labelo">N°</label>:<input type="number" min="0" name="numberstreet" class="space"
-        /><br>
-        <label for="labelStreet" class="labelo">Voie </label> :
-        <select name="labelStreet" id="labelStreet" class="spaceSelect">
-                    <option value="blanck" selected> </option>
-                    <option value="rue">Rue</option>
-                    <option value="ruelle">Ruelle</option>
-                    <option value="allee">Allée</option>
-                    <option value="impasse">Impasse</option>
-                    <option value="route">Route</option>
-                    <option value="rue">Avenue</option>
-                 </select> <br>
-        <label for="adress" class="labelo">Adresse</label> : <input type="text" name="adress" class="space" /><br>
-        <label for="city" class="labelo">Ville</label>:<input type="text" name="city" class="space" /> <br/>
-        <label for="country" class="labelo">Pays </label> :
-        <select name="country" id="country" class="spaceSelect">
-                    <option value="france" selected>France</option>
-                    <option value="allemagne">Allemagne</option>
-                    <option value="luxembourg">Luxembourg</option>
-                    <option value="belgique">Belgique</option>
-                    <option value="suisse">Suisse</option>
-                </select><br>
-        <label for="mail" class="labelo">Adresse mail</label>:<input type="email" name="email" class="space" /> <br/>
-        <label for="name" class="labelo">Téléphone</label>:<input type="text" name="name" class="space" /> <br>
-        <label for="name" class="labelo">Mot De Passe</label>:<input type="password" name="name" class="space" />
+      <form>
+        <div>
+          <label for="lastname" class="labelo">Nom</label>
+          <input type="text" name="lastname" class="space" />
+        </div>
+        <div>
+          <label for="firstname" class="labelo">Prénom</label>
+          <input type="text" name="firstname" class="space" />
+        </div>
+        <div>
+          <label for="numberstreet" class="labelo">N°</label>
+          <input type="number" min="0" name="numberstreet" class="space"/>
+        </div>
+        <div>
+          <label for="labelStreet" class="labelo">Voie</label>
+          <select name="labelStreet" id="labelStreet" class="spaceSelect">
+            <option value="blanck" selected> </option>
+            <option value="rue">Rue</option>
+            <option value="ruelle">Ruelle</option>
+            <option value="allee">Allée</option>
+            <option value="impasse">Impasse</option>
+            <option value="route">Route</option>
+            <option value="rue">Avenue</option>
+          </select>
+        </div>
+        <div>
+          <label for="adress" class="labelo">Adresse</label>
+          <input type="text" name="adress" class="space" />
+        </div>
+        <div>
+          <label for="city" class="labelo">Ville</label>
+          <input type="text" name="city" class="space" />
+        </div>
+        <div>
+          <label for="country" class="labelo">Pays</label>
+          <select name="country" id="country" class="spaceSelect">
+              <option value="france" selected>France</option>
+              <option value="allemagne">Allemagne</option>
+              <option value="luxembourg">Luxembourg</option>
+              <option value="belgique">Belgique</option>
+              <option value="suisse">Suisse</option>
+          </select>
+        </div>
+        <div>
+          <label for="mail" class="labelo">Adresse mail</label>
+          <input type="email" name="email" class="space" />
+        </div>
+        <div>
+          <label for="name" class="labelo">Téléphone</label>
+          <input type="text" name="name" class="space" />
+        </div>
+        <div>
+          <label for="name" class="labelo">Mot de passe</label>
+          <input type="password" name="name" class="space" />
+        </div>
+        <div>
+          <label for="name" class="labelo">Confirmation</label>
+          <input type="confirm" name="name" class="space" />
+        </div>
+
+        <input type="submit" value="S'inscrire" />
       </form>
-      <input type="submit" value="S'inscrire" />
   </div>
 </template>
 
 <script>
-export default {
-  name: 'header',
-  data () {
-    return {
-      msg: 'Hello world'
-    }
+  export default {
+    name: 'inscription'
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  label {
+    display: inline-block;
+    width: 250px;
+    text-align: left;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  input, select {
+    width: 250px;
+  }
 
-a {
-  color: #42b983;
-}
+  form div {
+    padding: 6px;
+  }
+
+  input[type=submit] {
+    margin-top: 10px;
+  }
 </style>
