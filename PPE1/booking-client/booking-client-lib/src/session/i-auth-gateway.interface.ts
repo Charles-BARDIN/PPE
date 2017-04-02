d8035ae46cb8a312012fc1f63a0245380a2c9851
Charles Bardin <c.bardin@aceforschool.com>
2017-04-02 05:39:47 +0200

    implement login and logout

--- PPE1/booking-client/booking-client-lib/src/session/i-auth-gateway.interface.ts
+++ PPE1/booking-client/booking-client-lib/src/session/i-auth-gateway.interface.ts
@@ -1,4 +1,6 @@
 import { User } from 'm2l-core';
 
 export interface IAuthGateway {
+  login(credentials: { mail: string, password: string }): Promise<User>
+  logout(userID: number): Promise<boolean>
 }
