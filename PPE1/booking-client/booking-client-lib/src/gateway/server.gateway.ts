d77ee134ce4a3f5426f2e0012807ec9d56b313d9
Charles Bardin <c.bardin@aceforschool.com>
2017-04-02 06:09:04 +0200

    implementing IAuthGateway

--- PPE1/booking-client/booking-client-lib/src/gateway/server.gateway.ts
+++ PPE1/booking-client/booking-client-lib/src/gateway/server.gateway.ts
@@ -1,7 +1,21 @@
+import { User } from 'm2l-core';
+
 import { IAuthGateway } from '../session';
 
-export class ServerGateway implements IAuthGateway{
+export class ServerGateway implements IAuthGateway {
   constructor() {
-    
+
+  }
+
+  public login(credentials: { mail: string, password: string }): Promise<User> {
+    return new Promise((resolve, reject) => {
+
+    });
+  }
+
+  public logout(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+
+    });
   }
 }
