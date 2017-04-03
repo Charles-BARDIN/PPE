36e2930c578ec2876b22576eaa05293834ddfd13
Charles Bardin <c.bardin@aceforschool.com>
2017-04-03 13:32:22 +0200

    mocked rooms

--- PPE1/booking-client/booking-client-app/src/lib-adapter/gateway.adapter.js
+++ PPE1/booking-client/booking-client-app/src/lib-adapter/gateway.adapter.js
@@ -1,3 +1,47 @@
 export default class Gateway {
+  constructor() {
 
+  }
+
+  login(credentials) {
+    return new Promise((resolve, reject) => {
+
+    });
+  }
+
+  logout() {
+    return new Promise((resolve, reject) => {
+
+    });
+  }
+
+  registerUser(user) {
+    return new Promise((resolve, reject) => {
+
+    });
+  }
+
+  getRooms() {
+    return new Promise((resolve, reject) => {
+      resolve([
+        {
+          id: 1,
+          label: 'Amphithéâtre',
+          img: ''
+        }
+      ]);
+    });
+  }
+
+  bookARoom(booking) {
+    return new Promise((resolve, reject) => {
+
+    });
+  }
+
+  modifyUser(user) {
+    return new Promise((resolve, reject) => {
+
+    });
+  }
 }
