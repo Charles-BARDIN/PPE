4fe7264d5788cadf54516604147d9a6cf1272902
Charles Bardin <c.bardin@aceforschool.com>
2017-04-04 15:31:38 +0200

    implement booking database adapter

--- PPE1/booking-server/booking-server-app/src/booking/booking-database.adapter.ts
+++ PPE1/booking-server/booking-server-app/src/booking/booking-database.adapter.ts
@@ -13,7 +13,10 @@ export class BookingDatabaseAdapter implements IBookingDataAccess {
 
   public add(booking: Booking): Promise<Booking> {
     return new Promise((resolve, reject) => {
-      this._db.query(``)
+      this._db.query(
+        `INSERT INTO booking(booking_date, userID, roomID)
+        VALUES (${booking.date}, ${booking.userID}, ${booking.roomID});`
+      )
         .then(() => {
           resolve();
         })
@@ -25,9 +28,17 @@ export class BookingDatabaseAdapter implements IBookingDataAccess {
 
   public get(booking: { roomID: number, date?: Date }): Promise<Booking[]> {
     return new Promise((resolve, reject) => {
-      this._db.query(``)
-        .then(() => {
-          resolve();
+      this._db.query(
+        `SELECT *
+        FROM booking;`
+      )
+        .then(bookings => {
+          let response = bookings
+            .reduce((prev, curr) => {
+              prev.push(new Booking(curr));
+              return prev;
+            }, []);
+          resolve(response);
         })
         .catch(err => {
           reject();
