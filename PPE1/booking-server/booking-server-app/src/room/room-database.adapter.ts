b69cf31f20f79d9116ad3229c598abf04de5cab0
Charles Bardin <c.bardin@aceforschool.com>
2017-04-04 15:22:24 +0200

    implement room database adapter

--- PPE1/booking-server/booking-server-app/src/room/room-database.adapter.ts
+++ PPE1/booking-server/booking-server-app/src/room/room-database.adapter.ts
@@ -13,12 +13,21 @@ export class RoomDatabaseAdapter implements IRoomDataAccess {
 
   public getRoom(id: number): Promise<Room> {
     return new Promise((resolve, reject) => {
-      this._db.query(``)
-        .then(() => {
-          resolve();
+      this._db.query(
+        `SELECT *
+        FROM room
+        WHERE room_id = ${id}`
+      )
+        .then(room => {
+          if(room.length) {
+            resolve(new Room(room[0]));
+            return;
+          }
+
+          reject('ERR_DB_ROOM_NOT_FOUND');
         })
         .catch(err => {
-          reject();
+          reject(err);
         })
     });
   }
