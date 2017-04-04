ec20ea69976c9c18f6d5fae141a5ce86ab40fe48
Charles Bardin <c.bardin@aceforschool.com>
2017-04-04 16:18:12 +0200

    add proxy to backend

--- PPE1/booking-client/booking-client-app/config/dev.env.js
+++ PPE1/booking-client/booking-client-app/config/dev.env.js
@@ -2,11 +2,5 @@ var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  proxyTable: {
-      '/api': {
-        target: 'http://localhost:9090',
-        changeOrigin: true
-      }
-    }
+  NODE_ENV: '"development"'
 })
