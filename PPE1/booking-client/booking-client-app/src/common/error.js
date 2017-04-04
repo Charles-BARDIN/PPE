d36dafc8eb5158daed560e2b44f61536035e0927
Charles Bardin <c.bardin@aceforschool.com>
2017-04-05 01:01:01 +0200

    add errors

--- PPE1/booking-client/booking-client-app/src/common/error.js
+++ PPE1/booking-client/booking-client-app/src/common/error.js
@@ -13,10 +13,12 @@ const errors = {
   ERR_BOOKING_DATE_REQUIRED: "Veuillez renseigner une date",
   ERR_BOOKING_DATE_PASSED: "La date sélectionnée est passée",
   ERR_BOOKING_USER_AUTH: "Vous n'êtes pas identifié",
+  ERR_ROOM_ALREADY_BOOKED: "Cette salle est déjà réservée à cette date",
 
   // Login
   ERR_LOGIN_MAIL_FORMAT: MAIL_FORMAT_MESSAGE,
   ERR_LOGIN_PASSWORD_REQUIRED: PASSWORD_REQUIRED_MESSAGE,
+  INVALID_CREDENTIALS: "Le mail et le mot de passe ne correspondent pas",
 
   // Profil
   ERR_MODIFYUSER_MAIL_FORMAT: MAIL_FORMAT_MESSAGE,
@@ -32,7 +34,10 @@ const errors = {
   ERR_REGISTER_COUNTRY_REQUIRED: "Veuillez renseigner votre pays",
   ERR_REGISTER_MAIL_FORMAT: MAIL_FORMAT_MESSAGE,
   ERR_REGISTER_PASSWORD_REQUIRED: PASSWORD_REQUIRED_MESSAGE,
-  ERR_REGISTER_PASSWORD_MATCH: CONFIRM_MATCH_MESSAGE
+  ERR_REGISTER_PASSWORD_MATCH: CONFIRM_MATCH_MESSAGE,
+
+  // Room
+  ERR_DB_ROOM_NOT_FOUND: "La liste des salles n'a pas pu être récupérée"
 };
 
 
