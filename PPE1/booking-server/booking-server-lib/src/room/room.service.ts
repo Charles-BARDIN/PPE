77d9566a93718cf774b0836158176e57a5f0a145
Charles Bardin <c.bardin@aceforschool.com>
2017-04-06 14:22:29 +0200

    add room service warn

--- PPE1/booking-server/booking-server-lib/src/room/room.service.ts
+++ PPE1/booking-server/booking-server-lib/src/room/room.service.ts
@@ -18,7 +18,12 @@ export class RoomService {
     this._logger.debug('RoomService.getRooms: called');
     return new Promise((resolve, reject) => {
       this._data.getRooms()
-        .then(resolve)
+        .then(rooms => {
+          if(!rooms) {
+            this._logger.warn('RoomService.getRooms: no rooms found')
+            reject('ERR_NO_ROOM_FOUND');
+          }
+        })
         .catch(err => {
           this._logger.error('RoomService.getRooms:', err);
           reject('ERR_UNKNOWN');
