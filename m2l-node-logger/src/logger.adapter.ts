57a9168cd2a8ecc4b44f5a593136fb799995e13f
Charles Bardin <c.bardin@aceforschool.com>
2017-04-06 12:26:34 +0200

    enrich logger

--- m2l-node-logger/src/logger.adapter.ts
+++ m2l-node-logger/src/logger.adapter.ts
@@ -4,23 +4,38 @@ class LoggerAdapter implements ILogger {
   constructor() { }
 
   public debug(...args: any[]) {
-    console.log.call(console, ...args);
+    let log = [];
+    log.push(...args);
+    log.unshift(`[${(new Date()).toLocaleDateString()} ${(new Date()).toLocaleTimeString()}] [DEBUG]:`)
+    console.log.call(console, ...log);
   }
 
   public log(...args: any[]) {
-    console.log.call(console, ...args);
+    let log = [];
+    log.push(...args);
+    log.unshift(`[${(new Date()).toLocaleDateString()} ${(new Date()).toLocaleTimeString()}] [LOG]:`)
+    console.log.call(console, ...log);
   }
 
   public info(...args: any[]) {
-    console.log.call(console, ...args);
+    let log = [];
+    log.push(...args);
+    log.unshift(`[${(new Date()).toLocaleDateString()} ${(new Date()).toLocaleTimeString()}] [INFO]:`)
+    console.log.call(console, ...log);
   }
 
   public warn(...args: any[]) {
-    console.log.call(console, ...args);
+    let log = [];
+    log.push(...args);
+    log.unshift(`[${(new Date()).toLocaleDateString()} ${(new Date()).toLocaleTimeString()}] [WARN]:`)
+    console.log.call(console, ...log);
   }
 
   public error(...args: any[]) {
-    console.log.call(console, ...args);
+    let log = [];
+    log.push(...args);
+    log.unshift(`[${(new Date()).toLocaleDateString()} ${(new Date()).toLocaleTimeString()}] [ERROR]:`)
+    console.log.call(console, ...log);
   }
 }
 
