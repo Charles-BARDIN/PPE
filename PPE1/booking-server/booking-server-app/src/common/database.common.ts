39ce7407e3b30c218b9f6a921c1f96dfbc4f248f
Charles Bardin <c.bardin@aceforschool.com>
2017-04-06 11:52:19 +0200

    move database creation to script

--- PPE1/booking-server/booking-server-app/src/common/database.common.ts
+++ PPE1/booking-server/booking-server-app/src/common/database.common.ts
@@ -18,57 +18,10 @@ export class Database {
       password: config.password,
       database: config.database
     });
-
-    this._init();
   }
 
   set database(database: string) {
     this._config.database = database;
-  }
-
-  private _init() {
-    this.query(
-      `CREATE TABLE IF NOT EXISTS user(
-        user_id        int (11) Auto_increment  NOT NULL ,
-        user_password  Varchar (255) NOT NULL ,
-        user_name      Varchar (255) NOT NULL ,
-        user_firstname Varchar (255) NOT NULL ,
-        user_mail      Varchar (255) NOT NULL ,
-        user_phone          Varchar (20)  ,
-        user_adresse   Varchar (255) NOT NULL ,
-        user_city           Varchar (255) NOT NULL ,
-        user_zip            Varchar (25) NOT NULL ,
-        user_country        Varchar (225) NOT NULL ,
-        PRIMARY KEY (user_id),
-        UNIQUE (user_mail)
-      )ENGINE=InnoDB;`
-    )
-      .then(() => {
-        return this.query(
-          `CREATE TABLE IF NOT EXISTS room(
-            room_id          int (11) Auto_increment  NOT NULL ,
-            room_label       Varchar(25) NOT NULL ,
-            room_description Text NOT NULL ,
-            room_image       Varchar (255) ,
-            PRIMARY KEY (room_id)
-          )ENGINE=InnoDB;`
-        );
-      })
-      .then(() => {
-        this.query(
-          `CREATE TABLE IF NOT EXISTS booking(
-            booking_date Date NOT NULL ,
-            user_id int NOT NULL,
-            room_id int NOT NULL,
-            PRIMARY KEY (room_id, booking_date),
-            CONSTRAINT FK_booking_user_id FOREIGN KEY (user_id) REFERENCES user(user_id),
-            CONSTRAINT FK_booking_room_id FOREIGN KEY (room_id) REFERENCES room(room_id)
-          )ENGINE=InnoDB;`
-        );
-      })
-      .catch(err => {
-        throw new Error(err.stack);
-      })
   }
 
   public query(sql: string): Promise<any[]> {
