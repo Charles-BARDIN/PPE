2bf962d5058dcf6e0bfe66b39c295d3aac5b94b2
Charles Bardin <c.bardin@aceforschool.com>
2017-04-08 13:00:13 +0200

    relative path fix

--- PPE2/admin-server/admin-server-app/config/server.config.js
+++ PPE2/admin-server/admin-server-app/config/server.config.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = {
   port: process.env.PORT || '9900',
-  roomImageRoot: process.env.ROOM_IMAGE_ROOT || process.env.NODE_ENV === 'production' ? '' : path.resolve(__dirname, '../../../../data/user_img'),
-  frontEndDirectory: process.env.STATIC_DIR || path.resolve(__dirname, '../../../../admin-client/admin-client-app/dist')
+  roomImageRoot: process.env.ROOM_IMAGE_ROOT || process.env.NODE_ENV === 'production' ? '' : path.resolve(__dirname, '../../../data/user_img'),
+  frontEndDirectory: process.env.STATIC_DIR || path.resolve(__dirname, '../../../admin-client/admin-client-app/dist')
 };
