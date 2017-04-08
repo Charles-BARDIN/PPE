9875647f232c8f37bda79b0f5fb85644abbce77a
Charles Bardin <c.bardin@aceforschool.com>
2017-04-08 12:01:14 +0200

    add generate config script

---
+++ PPE2/admin-client/admin-client-app/build/generate-config.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var path = require('path');
+
+var arg = process.argv[2];
+
+if(['dev', 'prod', 'mock'].indexOf(arg) < 0) {
+  throw new Error('Invalid argument');
+}
+
+fs.writeFileSync(path.resolve(__dirname, '../src/config.js'), 
+`export default {
+  apiBaseUrl: ${arg === 'dev' ? "'http://localhost:9000/api/v1.0.0'" : "'http://localhost:9900/api/v1.0.0'"},
+  isMock: ${arg === 'mock'}
+};`
+);
--- PPE2/admin-server/admin-server-app/config/server.config.ts
+++ PPE2/admin-server/admin-server-app/config/server.config.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 
 export const serverConfig = {
-  port: process.env.PORT || '9090',
+  port: process.env.PORT || '9900',
   roomImageRoot: process.env.ROOM_IMAGE_ROOT || process.env.NODE_ENV === 'production' ? '' : path.resolve(__dirname, '../../../../data/user_img'),
   frontEndDirectory: process.env.STATIC_DIR || path.resolve(__dirname, '../../../../admin-client/admin-client-app/dist')
 }
