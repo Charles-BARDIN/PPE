3277c1e3e4de30be51e4b8b0e3d8573552db3e80
Charles Bardin <c.bardin@aceforschool.com>
2017-04-08 10:59:10 +0200

    add auth service

---
+++ PPE2/admin-client/admin-client-lib/src/authentification/auth.service.ts
@@ -0,0 +1,53 @@
+import { User } from 'm2l-core';
+
+import { IAuthGateway } from '.';
+
+export class AuthService {
+  private _gateway: IAuthGateway;
+  private _user: User;
+  private _hash: Function;
+
+  constructor(config: {
+    gateway: IAuthGateway,
+    hash: Function
+  }) {
+    this._gateway = config.gateway;
+    this._hash = config.hash;
+  }
+
+  get user(): User {
+    return this._user;
+  }
+
+  get userID(): number {
+    return this._user.id;
+  }
+
+  public login(credentials: { mail: string, password: string }): Promise<boolean> {
+    return new Promise((resolve, reject) => {credentials.password = this._hash(credentials.password);
+      this._gateway.login(credentials)
+        .then(user => {
+          this._user = user;
+          resolve(true);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    })
+  }
+
+  public logout(): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this._gateway.logout(this._user.id)
+        .then(res => {
+          this._user = undefined;
+          resolve(true);
+        })
+        .catch(err => reject(err));
+    })
+  }
+
+  public userIsConnected(): boolean {
+    return this._user != null;
+  }
+}
---
+++ PPE2/admin-client/admin-client-lib/src/authentification/index.ts
@@ -0,0 +1,2 @@
+export * from './auth.service';
+export * from './i-auth-gateway.interface';
