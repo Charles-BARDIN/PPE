ab835353c32a7de6e13f2b4c21021ec508112bb8
Charles BARDIN <bardincharles@gmail.com>
2017-04-10 14:42:56 +0200

    implement login state

---
+++ PPE2/admin-client/admin-client-lib/src/login-state/i-login-validator.interface.ts
@@ -0,0 +1,3 @@
+export interface ILoginValidator {
+  validateMail(mail: string): boolean;
+}
---
+++ PPE2/admin-client/admin-client-lib/src/login-state/index.ts
@@ -0,0 +1,5 @@
+export * from './login-state.service';
+export * from './i-login-auth-access.interface';
+export * from './i-login-validator.interface';
+export * from './i-login-controller.interface';
+export * from './i-login-nav-access.interface';
