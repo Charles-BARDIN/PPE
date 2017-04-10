51e98f9a36eae931bcc923b16185ebbe80acef9d
Charles BARDIN <bardincharles@gmail.com>
2017-04-10 16:52:59 +0200

    add room service

---
+++ PPE2/admin-client/admin-client-lib/src/room-state/room-state.service.ts
@@ -0,0 +1,43 @@
+import { Room } from 'm2l-core';
+
+import { IRoomNavAccess, IRoomGateway, IRoomController } from '.';
+
+export class RoomService {
+    private _nav: IRoomNavAccess;
+    private _gateway: IRoomGateway;
+    private _controller: IRoomController
+
+    constructor(config: {
+        navigation: IRoomNavAccess,
+        gateway: IRoomGateway
+    }) {
+        this._nav = config.navigation;
+        this._gateway = config.gateway;
+    }
+
+    set controller(controller: IRoomController) {
+        this._controller = controller;
+    }
+
+    public onPageLoad() {
+        this._gateway.getAllRooms()
+            .then(rooms => {
+                this._controller.setRoomList(rooms);
+            })
+            .catch(errors => {
+                this._controller.showErrors(errors);
+            })
+    }
+
+    public onEditClick(room: Room) {
+        this._nav.goTo('room-edit', room);
+    }
+
+    public onRemoveClick(room: Room) {
+        this._nav.goTo('room-delete', room);
+    }
+
+    public onNewClick() {
+        this._nav.goTo('room-new');
+    }
+}
