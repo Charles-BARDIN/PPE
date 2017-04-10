ae1d59b8af420844e6421cc35373577065c48812
Charles BARDIN <bardincharles@gmail.com>
2017-04-10 14:45:42 +0200

    format file

--- PPE2/admin-client/admin-client-lib/src/login-state/login-state.service.ts
+++ PPE2/admin-client/admin-client-lib/src/login-state/login-state.service.ts
@@ -4,45 +4,45 @@ import { ILoginController } from '.';
 import { ILoginNavAccess } from '.';
 
 export class LoginService {
-  private _auth: ILoginAuthAccess;
-  private _nav: ILoginNavAccess;
-  private _validator: ILoginValidator;
-  private _controller: ILoginController;
+    private _auth: ILoginAuthAccess;
+    private _nav: ILoginNavAccess;
+    private _validator: ILoginValidator;
+    private _controller: ILoginController;
 
-  constructor(config: {
-    authentification: ILoginAuthAccess,
-    navigation: ILoginNavAccess,
-  }) {
-    this._auth = config.authentification;
-    this._nav = config.navigation;
-  }
-
-  set controller (controller: ILoginController) {
-    this._controller = controller;
-  }
-
-  set validator(validator: ILoginValidator) {
-    this._validator = validator;
-  }
-
-  public login(credentials: { mail: string, password: string }) {
-    let errors = [];
-
-    if(!credentials.mail || ! this._validator.validateMail(credentials.mail)) {
-      errors.push('ERR_LOGIN_MAIL_FORMAT');
+    constructor(config: {
+        authentification: ILoginAuthAccess,
+        navigation: ILoginNavAccess,
+    }) {
+        this._auth = config.authentification;
+        this._nav = config.navigation;
     }
 
-    if(!credentials.password) {
-      errors.push('ERR_LOGIN_PASSWORD_REQUIRED');
+    set controller(controller: ILoginController) {
+        this._controller = controller;
     }
 
-    if(errors.length) {
-      this._controller.showValidationErrors(errors);
-      return;
+    set validator(validator: ILoginValidator) {
+        this._validator = validator;
     }
 
-    this._auth.login(credentials)
-      .then(res => this._nav.goTo('room'))
-      .catch(err => this._controller.showBackendErrors(err))
-  }
+    public login(credentials: { mail: string, password: string }) {
+        let errors = [];
+
+        if (!credentials.mail || !this._validator.validateMail(credentials.mail)) {
+            errors.push('ERR_LOGIN_MAIL_FORMAT');
+        }
+
+        if (!credentials.password) {
+            errors.push('ERR_LOGIN_PASSWORD_REQUIRED');
+        }
+
+        if (errors.length) {
+            this._controller.showValidationErrors(errors);
+            return;
+        }
+
+        this._auth.login(credentials)
+            .then(res => this._nav.goTo('room'))
+            .catch(err => this._controller.showBackendErrors(err))
+    }
 }
