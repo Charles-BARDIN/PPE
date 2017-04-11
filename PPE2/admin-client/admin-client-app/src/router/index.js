c786a8a9fe453b31a340bef8149201534d9ea4ae
Charles Bardin <c.bardin@aceforschool.com>
2017-04-11 20:30:24 +0200

    add vue component and router

--- PPE2/admin-client/admin-client-app/src/router/index.js
+++ PPE2/admin-client/admin-client-app/src/router/index.js
@@ -1,15 +1,47 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Login from '@/components/Login'
+import Main from '@/components/Main'
+import Booking from '@/components/Booking'
+import NewRoom from '@/components/NewRoom'
+import EditRoom from '@/components/EditRoom'
+import Room from '@/components/Room'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+    {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Main',
+      component: Main,
+      children: [
+        {
+          path: '/booking',
+          name: 'booking',
+          component: Booking
+        },
+        {
+          path: '/edit-room',
+          name: 'Edit Room',
+          component: EditRoom
+        },
+        {
+          path: '/room',
+          name: 'Room',
+          component: Room
+        },
+        {
+          path: '/new-room',
+          name: 'New Room',
+          component: NewRoom
+        }
+      ]
     }
   ]
 })
