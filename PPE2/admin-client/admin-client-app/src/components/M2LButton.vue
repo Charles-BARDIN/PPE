e92855814407b0d17ea2115cdecd75532326f584
Charles Bardin <c.bardin@aceforschool.com>
2017-04-11 20:19:12 +0200

    add commmon components

---
+++ PPE2/admin-client/admin-client-app/src/components/M2LButton.vue
@@ -0,0 +1,28 @@
+<template>
+  <p class="m2l-button">
+    <a role="button" v-on:click="action">{{ label }}</a>
+  </p>
+</template>
+
+<script>
+  export default {
+    name: 'm2l-button',
+    props: ['label', 'action']
+  }
+</script>
+
+<style scoped>
+  a {
+    color: #42b983;
+  }
+
+  p {
+    display: inline-block;
+    padding: 6px 16px;
+    background-color: rgba(0, 0, 125, 0.5);
+    box-shadow: 5px 5px rgba(0, 0, 0, 0.8);
+    border-radius: 10px;
+    cursor: pointer;
+    user-select: none;
+  }
+</style>
---
+++ PPE2/admin-client/admin-client-app/src/components/RoomPicker.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <select class="room-picker" id="room" v-model="selectedRoom">
+      <option v-for="room in rooms">{{ room.name }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'room-picker',
+    data () {
+      return {
+        selectedRoom: {}
+      }
+    },
+    watch: {
+      selectedRoom: function(newRoom) {
+        this.onSelectedRoomChange(this.rooms.filter(room => room.name === newRoom)[0]);
+      }
+    },
+    props: ['onSelectedRoomChange', 'rooms']
+  }
+</script>
+
+<style scoped>
+</style>
