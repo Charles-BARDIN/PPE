5502e2b8d73fb9b131bca24135d5a6173d0dbb72
Charles Bardin <c.bardin@aceforschool.com>
2017-04-19 13:16:37 +0200

    add NewRoom feature

---
+++ PPE2/admin-client/admin-client-app/src/components/NewRoom.vue
@@ -0,0 +1,94 @@
+<template>  
+  <div>
+    <RoomForm mode="new" v-bind:on-add-click="onAddClick" v-bind:on-cancel-click="onCancelClick"/>
+
+    <Error>
+      <div v-for="error in errors">
+        {{ error }}
+      </div>
+    </Error>
+
+    <Confirm v-if="showConfirm">
+      Salle ajoutée avec succès !
+    </Confirm>
+  </div>
+</template>
+
+<script>
+import Error from '@/components/Error';
+import Confirm from '@/components/Confirm';
+import RoomForm from '@/components/RoomForm';
+
+import adminClientLib from '@/lib-adapters';
+
+const roomNewService = adminClientLib.roomNewService;
+
+export default {
+  name: 'new-room',
+  components: {
+    Error,
+    Confirm,
+    RoomForm
+  },
+  created: function() {
+    const controller = {
+      showErrors: this.showErrors,
+      showConfirmation: this.showConfirmation,
+      hideMessages: this.hideMessages
+    };
+
+    roomNewService.controller = controller;
+  },
+  data () {
+    return {
+      errors: [],
+      showConfirm: false
+    }
+  },
+  methods: {
+    onAddClick: function(room) {
+      roomNewService.addRoom(room);
+    },
+    onCancelClick: function() {
+      roomNewService.cancel();
+    },
+    showErrors,
+    showConfirmation,
+    hideMessages
+  }
+};
+
+function showErrors(errors) {
+  this.errors = errors;
+}
+
+function showConfirmation() {
+  this.showConfirm = true;
+}
+
+function hideMessages() {
+  this.errors = [];
+  this.showConfirm = false;
+}
+</script>
+
+<style scoped>
+  label {
+    display: inline-block;
+    width: 250px;
+    text-align: left;
+  }
+
+  input, textarea {
+    width: 250px;
+    max-width: 250px;
+  }
+
+  form {
+    margin-top: 10px;
+  }
+
+  form div {
+    padding: 6px;
+  }
+</style>
--- PPE2/admin-client/admin-client-app/src/lib-adapters/mocked-gateway.adapter.js
+++ PPE2/admin-client/admin-client-app/src/lib-adapters/mocked-gateway.adapter.js
@@ -66,7 +66,7 @@ export default class MockedGateway {
 
   addRoom(room) {
     return new Promise(resolve => {
-
+      resolve(true);
     });
   }
 
