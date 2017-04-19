22f98399c6ae2299d86970bb153bbd99740835a7
Charles Bardin <c.bardin@aceforschool.com>
2017-04-19 22:22:08 +0200

    add caches to mock

--- PPE2/admin-client/admin-client-app/src/lib-adapters/mocked-gateway.adapter.js
+++ PPE2/admin-client/admin-client-app/src/lib-adapters/mocked-gateway.adapter.js
@@ -1,5 +1,37 @@
 export default class MockedGateway {
-  constructor() { }
+  constructor() {
+    this._bookings = [
+      {
+        roomID: 1,
+        userID: 1,
+        date: new Date(),
+        userMail: 'mail@example.com',
+        roomName: 'Amphithéâtre'
+      },
+      {
+        roomID: 1,
+        userID: 2,
+        date: new Date(Number(new Date()) - 1000 * 3600 * 24 * 2),
+        userMail: 'other@example.com',
+        roomName: 'Salle conviviale'
+      }
+    ];
+
+    this._rooms = [
+      {
+        id: 1,
+        name: 'Amphithéâtre',
+        image: '',
+        description: 'Amphi description'
+      },
+      {
+        id: 2,
+        name: 'Salle conviviale',
+        image: '',
+        description: 'Salle conviviale'
+      }
+    ];
+  }
 
   login(credentials) {
     return Promise.resolve({
@@ -20,30 +52,30 @@ export default class MockedGateway {
   }
 
   cancelBooking(booking) {
-    return Promise.resolve();
+    const index = this._findBooking(booking);
+
+    if (index < 0) {
+      return Promise.resolve(true);
+    }
+
+    this._bookings.splice(index, 1);
+    return Promise.resolve(true);
   }
 
   getAllBookings() {
-    return Promise.resolve([
-      {
-        roomID: 1,
-        userID: 1,
-        date: new Date(),
-        userMail: 'mail@example.com',
-        roomName: 'Amphithéâtre'
-      },
-      {
-        roomID: 1,
-        userID: 1,
-        date: new Date(Number(new Date()) - 1000 * 3600 * 24 * 2),
-        userMail: 'mail@example.com',
-        roomName: 'Amphithéâtre'
-      }
-    ]);
+    return Promise.resolve(this._bookings);
   }
 
   deleteRoom(room) {
-    return Promise.resolve();
+    const index = this._findRoom(room);
+
+    if (index < 0) {
+      return Promise.resolve(true);
+    }
+
+    this._rooms.splice(index, 1);
+
+    return Promise.resolve(true);
   }
 
   getRoomImage(room) {
@@ -51,22 +83,50 @@ export default class MockedGateway {
   }
 
   modifyRoom(room) {
-    return Promise.resolve({});
+    const index = this._findRoom(room);
+
+    if (index < 0) {
+      return Promise.resolve(room);
+    }
+
+    this._rooms[index].description = room.description;
+    this._rooms[index].image = room.image;
+    this._rooms[index].name = room.name;
+
+    return Promise.resolve(this._rooms[index]);
   }
 
   addRoom(room) {
-    return Promise.resolve(true);
+    room.id = this._rooms[this._rooms.length - 1].id + 1;
+    this._rooms.push(room);
+    return Promise.resolve(this._rooms[this._rooms.length - 1]);
   }
 
   getAllRooms() {
-    return Promise.resolve([
-      {
-        id: 1,
-        name: 'Amphithéâtre',
-        image: '',
-        description: 'Amphi description'
-      }
-    ]);
+    return Promise.resolve(this._rooms);
   }
 
+  _findBooking(booking) {
+    return this._bookings
+      .findIndex(book => {
+        book.date.setHours(0);
+        book.date.setMinutes(0);
+        book.date.setSeconds(0);
+        book.date.setMilliseconds(0);
+
+        booking.date.setHours(0);
+        booking.date.setMinutes(0);
+        booking.date.setSeconds(0);
+        booking.date.setMilliseconds(0);
+
+        return Number(book.date) === Number(booking.date) && book.roomID === booking.roomID;
+      });
+  }
+
+  _findRoom(room) {
+    return this._rooms
+      .findIndex(roomFromArray => {
+        return Number(room.id) === Number(roomFromArray.date);
+      });
+  }
 }
