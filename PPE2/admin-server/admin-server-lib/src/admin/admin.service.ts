03bf4f133f07aba7e62a26340c4e2073fe38cdb6
Charles Bardin <c.bardin@aceforschool.com>
2017-04-25 19:01:09 +0200

    implement admin service

--- PPE2/admin-server/admin-server-lib/src/admin/admin.service.ts
+++ PPE2/admin-server/admin-server-lib/src/admin/admin.service.ts
@@ -16,7 +16,37 @@ export class AdminService {
 
   public login(credentials: { mail: string, password: string }): Promise<boolean> {
     return new Promise((resolve, reject) => {
+      if (!credentials) {
+        reject('ADMIN_LOGIN_REQUIRED_CREDENTIALS');
+      }
+      let errors = [];
 
+      if (!credentials.mail) {
+        errors.push('ADMIN_LOGIN_REQUIRED_MAIL');
+      }
+
+      if (!credentials.password) {
+        errors.push('ADMIN_LOGIN_REQUIRED_PASSWORD');
+      }
+
+      if (errors.length) {
+        reject(errors);
+        return;
+      }
+
+      this._data.getAdminByCredentials(credentials)
+        .then(admin => {
+          if (!admin) {
+            reject('INVALID_CREDENTIALS');
+            return;
+          }
+
+          resolve(admin);
+        })
+        .catch(errors => {
+          this._logger.error(errors);
+          reject('UNKNOWN_ERROR');
+        })
     });
   }
 
