a61f6a11219c66027e79b6892467426fbc2100d1
Charles-BARDIN <charles.bardin.pro@gmail.com>
2017-04-27 22:14:45 +0200

    returning Promises

--- PPE2/admin-client/admin-client-app/src/lib-adapters/gateway.adapter.js
+++ PPE2/admin-client/admin-client-app/src/lib-adapters/gateway.adapter.js
@@ -1,4 +1,4 @@
-import { User, Booking, Room } from 'm2l-core';
+import { Admin, Booking, Room } from 'm2l-core';
 
 export default class Gateway {
   constructor(baseUrl) {
@@ -6,125 +6,143 @@ export default class Gateway {
   }
 
   login(credentials) {
-    return this._http(`POST`, `/login`, credentials)
-      .then(res => {
-        if (res.faults.length) {
-          reject(res.faults);
-          return;
-        }
-        resolve(new User(res.data));
-      })
-      .catch(err => reject(err));
+    return new Promise((resolve, reject) => {
+      this._http(`POST`, `/login`, credentials)
+        .then(res => {
+          if (res.faults.length) {
+            reject(res.faults);
+            return;
+          }
+          resolve(new Admin(res.data));
+        })
+        .catch(err => reject(err));
+    });
   }
 
   logout(userID) {
-    return this._http(`POST`, `/logout`)
-      .then(res => {
-        if (res.faults.length) {
-          reject(res.faults);
-          return;
-        }
-        resolve();
-      })
-      .catch(err => reject(err));
+    return new Promise((resolve, reject) => {
+      this._http(`POST`, `/logout`)
+        .then(res => {
+          if (res.faults.length) {
+            reject(res.faults);
+            return;
+          }
+          resolve();
+        })
+        .catch(err => reject(err));
+    });
   }
 
   cancelBooking(booking) {
-    return this._http(`DELETE`, `/booking/${booking.id}`, booking)
-      .then(res => {
-        if (res.faults.length) {
-          reject(res.faults);
-          return;
-        }
-        resolve(new Booking(res.data));
-      })
-      .catch(err => {
-        reject(err);
-      });
+    return new Promise((resolve, reject) => {
+      this._http(`DELETE`, `/booking/${booking.id}`, booking)
+        .then(res => {
+          if (res.faults.length) {
+            reject(res.faults);
+            return;
+          }
+          resolve(new Booking(res.data));
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
   }
 
   getAllBookings() {
-    return this._http(`GET`, `/booking`)
-      .then(res => {
-        if (res.faults.length) {
-          reject(res.faults);
-          return;
-        }
-        resolve(res.data.map(book => new Booking(book)));
-      })
-      .catch(err => {
-        reject(err);
-      });
+    return new Promise((resolve, reject) => {
+      this._http(`GET`, `/booking`)
+        .then(res => {
+          if (res.faults.length) {
+            reject(res.faults);
+            return;
+          }
+          resolve(res.data.map(book => new Booking(book)));
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
   }
 
   deleteRoom(room) {
-    return this._http(`DELETE`, `/room/${room.id}`)
-      .then(res => {
-        if (res.faults.length) {
-          reject(res.faults);
-          return;
-        }
-        resolve(res.data);
-      })
-      .catch(err => {
-        reject(err);
-      });
+    return new Promise((resolve, reject) => {
+      this._http(`DELETE`, `/room/${room.id}`)
+        .then(res => {
+          if (res.faults.length) {
+            reject(res.faults);
+            return;
+          }
+          resolve(res.data);
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
   }
 
   modifyRoom(room) {
-    return this._http(`PUT`, `/room/${room.id}`, room)
-      .then(res => {
-        if (res.faults.length) {
-          reject(res.faults);
-          return;
-        }
-        resolve(new Room(res.data));
-      })
-      .catch(err => {
+    return new Promise((resolve, reject) => {
+      this._http(`PUT`, `/room/${room.id}`, room)
+        .then(res => {
+          if (res.faults.length) {
+            reject(res.faults);
+            return;
+          }
+          resolve(new Room(res.data));
+        })
+        .catch(err => {
 
-      });
+        });
+    });
   }
 
   addRoom(room) {
-    return this._http(`POST`, `/room`, room)
-      .then(res => {
-        if (res.faults.length) {
-          reject(res.faults);
-          return;
-        }
-        resolve(new Room(res.data));
-      })
-      .catch(err => {
+    return new Promise((resolve, reject) => {
+      this._http(`POST`, `/room`, room)
+        .then(res => {
+          if (res.faults.length) {
+            reject(res.faults);
+            return;
+          }
+          resolve(new Room(res.data));
+        })
+        .catch(err => {
 
-      });
+        });
+    });
   }
 
   getAllRooms() {
-    return this._http(`GET`, `/room`)
-      .then(res => {
-        if (res.faults.length) {
-          reject(res.faults);
-          return;
-        }
-        resolve(res.data.map(room => new Room(room)));
-      })
-      .catch(err => {
+    return new Promise((resolve, reject) => {
+      this._http(`GET`, `/room`)
+        .then(res => {
+          if (res.faults.length) {
+            reject(res.faults);
+            return;
+          }
+          resolve(res.data.map(room => new Room(room)));
+        })
+        .catch(err => {
 
-      });
+        });
+    });
   }
 
   getRoomImage(room) {
-    return this._http(`GET`, `/room/${room.id}/image`)
-      .then(res => {
-        if (res.faults.length) {
-          reject(res.faults);
-          return;
-        }
-        resolve(res.data);
-      })
-      .catch(err => {
+    return new Promise((resolve, reject) => {
+      this._http(`GET`, `/room/${room.id}/image`)
+        .then(res => {
+          if (res.faults.length) {
+            reject(res.faults);
+            return;
+          }
+          resolve(res.data);
+        })
+        .catch(err => {
 
-      });
+        });
+    });
   }
 
   _http(verb, url, argument) {
@@ -134,6 +152,7 @@ export default class Gateway {
 
       req.onreadystatechange = function (event) {
         if (this.readyState === XMLHttpRequest.DONE) {
+
           if ([200, 304].indexOf(this.status) !== -1) {
             let rep;
             try {
