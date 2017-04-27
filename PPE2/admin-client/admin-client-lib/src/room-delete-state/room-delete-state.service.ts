6d6a97f99ee69a2296c1a959022dddb84ecc1c06
metaxiii <lehchibi.gael@gmail.com>
2017-04-27 19:47:57 +0200

    Add auth deleteroom

--- PPE2/admin-client/admin-client-lib/src/room-delete-state/room-delete-state.service.ts
+++ PPE2/admin-client/admin-client-lib/src/room-delete-state/room-delete-state.service.ts
@@ -1,19 +1,23 @@
 import { Room } from 'm2l-core';
 
-import { IRoomDeleteNavAccess, IRoomDeleteGateway, IRoomDeleteController } from '.';
+import { IRoomDeleteNavAccess, IRoomDeleteGateway, IRoomDeleteController,IRoomDeleteRoomAuth } from '.';
 
 export class RoomDeleteService {
   private _nav: IRoomDeleteNavAccess;
   private _gateway: IRoomDeleteGateway;
   private _controller: IRoomDeleteController;
   private _room: Room;
+  private _auth : IRoomDeleteRoomAuth ; 
+
 
   constructor(config: {
     navigation: IRoomDeleteNavAccess,
-    gateway: IRoomDeleteGateway
+    gateway: IRoomDeleteGateway,
+    authentification : IRoomDeleteRoomAuth
   }) {
     this._nav = config.navigation;
     this._gateway = config.gateway;
+    this._auth = config.authentification ; 
 
     this._room = {
       description: undefined,
@@ -32,6 +36,9 @@ export class RoomDeleteService {
   }
 
   public onPageLoad() {
+    if(!this._auth.userIsConnected()){
+      this._nav.goTo('login'); 
+    }
     const room = this._nav.getRouteParameters();
 
     for (let key in room) {
