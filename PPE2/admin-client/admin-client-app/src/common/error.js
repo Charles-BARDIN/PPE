ecf11cfa0cab0bb2ecd0afc84c66291c5cf94d54
Charles-BARDIN <charles.bardin.pro@gmail.com>
2017-04-27 22:00:02 +0200

    error handling structure

---
+++ PPE2/admin-client/admin-client-app/src/common/error.js
@@ -0,0 +1,22 @@
+const DEFAULT_MESSAGE = "Une erreur inconnue est arrivée";
+
+const errors = {
+  // Login
+  ERR_LOGIN_MAIL_FORMAT: "",
+  ERR_LOGIN_PASSWORD_REQUIRED: "",
+
+  // Room
+  ERR_EDITROOM_DESCRIPTION_REQUIRED: "",
+  ERR_EDITROOM_NAME_REQUIRED: "",
+  ERR_NEWROOM_DESCRIPTION_REQUIRED: "",
+  ERR_NEWROOM_NAME_REQUIRED: "",
+
+  // Booking
+  ERR_ROOM_BOOKED: ""
+};
+
+
+export {
+  errors,
+  DEFAULT_MESSAGE
+};
