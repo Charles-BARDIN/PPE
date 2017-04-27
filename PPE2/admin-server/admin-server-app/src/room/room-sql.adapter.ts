96fca40360295f3480823eff261f33f70b3415a2
Charles Bardin <c.bardin@aceforschool.com>
2017-04-27 13:30:25 +0200

    add structure and dependencies

--- PPE2/admin-server/admin-server-app/src/room/room-sql.adapter.ts
+++ PPE2/admin-server/admin-server-app/src/room/room-sql.adapter.ts
@@ -1,3 +1,53 @@
-export class RoomSQLAdapter {
-  
+import { Room, ILogger } from 'm2l-core';
+
+import { IRoomDataAccess } from 'admin-server-lib';
+
+import { Database } from '../common';
+
+export class RoomSQLAdapter implements IRoomDataAccess {
+  private _db: Database;
+  private _logger: ILogger;
+
+  constructor(config: { database: Database, logger: ILogger }) {
+    this._db = config.database;
+    this._logger = config.logger;
+  }
+
+  deleteRoom(id: number): Promise<boolean> {
+
+  }
+
+  modifyRoom(room: Room): Promise<Room> {
+
+  }
+
+  addRoom(room: Room): Promise<Room> {
+
+  }
+
+  getRooms(): Promise<Room[]> {
+
+  }
+
+  getRoomImage(id: number): Promise<{ ext: string, data: string }> {
+    
+  }
+
+  private _escapeHtml(unsafe: string): string {
+    return unsafe
+      .replace(/&/g, "&amp;")
+      .replace(/</g, "&lt;")
+      .replace(/>/g, "&gt;")
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&#039;");
+  }
+
+  private _unescapeHtml(safe: string): string {
+    return safe
+      .replace(/&amp;/g, "&")
+      .replace(/&lt;/g, "<")
+      .replace(/&gt;/g, ">")
+      .replace(/&quot;/g, '"')
+      .replace(/&#039;/g, "'");
+  }
 }
--- PPE2/admin-server/admin-server-app/src/room/room.adapter.ts
+++ PPE2/admin-server/admin-server-app/src/room/room.adapter.ts
@@ -1,3 +1,25 @@
+import { ILogger, Admin } from 'm2l-core';
+
+import { RoomService } from 'admin-server-lib';
+import { RoomSQLAdapter } from '.';
+import { Database } from '../common';
+
 export class RoomAdapter {
-  
+  private _logger: ILogger;
+  private _roomService: RoomService;
+
+  constructor(config: { logger: ILogger, database: Database }) {
+    const dataAccess = new RoomSQLAdapter({ logger: config.logger, database: config.database });
+
+    this._roomService = new RoomService({ logger: config.logger, dataAccess });
+    this._logger = config.logger;
+  }
+
+  private _handleErrors(errors, reject) {
+    if (!Array.isArray(errors)) {
+      errors = [errors];
+    }
+
+    reject(errors || []);
+  }
 }
