9011df017674f2b055a54f5767f247623b6c2e31
metaxiii <lehchibi.gael@gmail.com>
2017-04-27 19:57:52 +0200

    Add auth deleteRoom exports

--- PPE2/admin-client/admin-client-lib/src/room-delete-state/index.ts
+++ PPE2/admin-client/admin-client-lib/src/room-delete-state/index.ts
@@ -2,3 +2,4 @@ export * from './room-delete-state.service';
 export * from './i-room-delete-nav-access.interface';
 export * from './i-room-delete-controller.interface';
 export * from './i-room-delete-gateway.interface';
+export * from './i-room-delete-auth.interface';
