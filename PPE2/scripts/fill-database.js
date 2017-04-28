e71851d07c237e05f65d16f39eea50f4a60cb3e3
Charles-BARDIN <charles.bardin.pro@gmail.com>
2017-04-28 03:32:21 +0200

    database fill

--- PPE2/scripts/fill-database.js
+++ PPE2/scripts/fill-database.js
@@ -1,4 +1,5 @@
 const mySql = require('mysql');
+const escape = mySql.escape;
 
 const config = {
   host: process.env.DB_HOST || 'localhost',
@@ -28,18 +29,33 @@ connection.connect(err => {
         process.exit(1);
       }
 
+      const amphiDescr = escape(`D'une capacité de 100 personnes, nos amphithéatre vous permettront de réserver une grande salle pour vos employés.`);
+      const reunionDescr = escape(`D'une capacité de 20 personnes, nos salles de réunions vous permettront d'organiser vos activités`);
+      const convivDescr = escape(`Les salles de convialité vous permettront de déjeuner dans les locaux de la Maison des Ligues ainsi que de vous reposer.`);
+
       connection.query(
-        `INSERT INTO room(room_label, room_description, room_image)
-        VALUES('Amphithéâtre', 'Amphithéâtre description', ''), ('Salle de réunion', 'Salle de réunion description', ''), ('Salle de convivialité', 'Salle de convivialité description', '');`,
+        `INSERT INTO room(room_label, room_description)
+        VALUES('Amphithéâtre', ${amphiDescr}), ('Salle de réunion', ${reunionDescr}), ('Salle de convivialité', ${convivDescr});`,
         err => {
           if (err) {
             console.error(err);
             process.exit(1);
-          } 
-          
-          console.log(`Database ${config.database} filled with success`);
-          process.exit(0);
+          }
 
+          connection.query(
+            `INSERT INTO admin(admin_mail, admin_password)
+            VALUES('a@a.a', '8333a9942239703018ac066ec994ea1ab21caf50bc413b63fb345e181f7c3487');`,
+            // Password a@a.a
+            err => {
+              if (err) {
+                console.error(err);
+                process.exit(1);
+              }
+
+              console.log(`Database ${config.database} filled with success`);
+              process.exit(0);
+            }
+          );
         }
       );
     }
