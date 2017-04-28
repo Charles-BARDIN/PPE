f0d4afeb91bc963f4a4e7b0691f57e3d37d12942
Charles-BARDIN <charles.bardin.pro@gmail.com>
2017-04-28 15:03:17 +0200

    add api logs

--- PPE2/admin-server/admin-server-app/src/server/express.api.ts
+++ PPE2/admin-server/admin-server-app/src/server/express.api.ts
@@ -44,109 +44,136 @@ export class ExpressApi {
 
   private _setApi() {
     this._router.post('/login', (req, res) => {
+      this._logger.log('ExpressApi: request POST on /login', req.body);
       this._admin.login(req.body)
         .then(data => {
           const response = { faults: [], data };
+          this._logger.log('ExpressApi: request POST on /login, response:', response);
           res.send(response);
         })
         .catch(faults => {
           const response = { faults };
+          this._logger.log('ExpressApi: request POST on /login, response:', response);
           res.send(response);
         });
     });
 
     this._router.post('/logout', (req, res) => {
+      this._logger.log('ExpressApi: request POST on /logout');
       this._admin.logout()
         .then(data => {
           const response = { faults: [], data };
+          this._logger.log('ExpressApi: request POST on /logout, response:', response);
           res.send(response);
         })
         .catch(faults => {
           const response = { faults };
+          this._logger.log('ExpressApi: request POST on /logout, response:', response);
           res.send(response);
         });
     });
 
     this._router.delete('/booking/:id', (req, res) => {
+      this._logger.log('ExpressApi: request DELETE on /booking/' + req.params.id, req.body);
       this._booking.cancelBooking(req.body)
         .then(data => {
           const response = { faults: [], data };
+          this._logger.log('ExpressApi: request DELETE on /booking/' + req.params.id + ', response:', response);
           res.send(response);
         })
         .catch(faults => {
           const response = { faults };
+          this._logger.log('ExpressApi: request DELETE on /booking/' + req.params.id + ', response:', response);
           res.send(response);
         });
     });
 
     this._router.get('/booking', (req, res) => {
+      this._logger.log('ExpressApi: request GET on /booking');
       this._booking.getAllBookings()
         .then(data => {
           const response = { faults: [], data };
+          this._logger.log('ExpressApi: request GET on /booking, response:', response);
           res.send(response);
         })
         .catch(faults => {
           const response = { faults };
+          this._logger.log('ExpressApi: request GET on /booking, response:', response);
           res.send(response);
         });
     });
 
     this._router.delete('/room/:id', (req, res) => {
+      this._logger.log('ExpressApi: request DELETE on /room/' + req.params.id);
       this._room.deleteRoom(req.params.id)
         .then(data => {
           const response = { faults: [], data };
+          this._logger.log('ExpressApi: request DELETE on /room/' + req.params.id + ', response:', response);
           res.send(response);
         })
         .catch(faults => {
           const response = { faults };
+          this._logger.log('ExpressApi: request DELETE on /room/' + req.params.id + ', response:', response);
           res.send(response);
         });
     });
 
     this._router.put('/room/:id', (req, res) => {
+      this._logger.log('ExpressApi: request PUT on /room/' + req.params.id, req.body);
       this._room.modifyRoom(req.body)
         .then(data => {
           const response = { faults: [], data };
+          this._logger.log('ExpressApi: request PUT on /room/' + req.params.id + ', response:', response);
           res.send(response);
         })
         .catch(faults => {
           const response = { faults };
+          this._logger.log('ExpressApi: request PUT on /room/' + req.params.id + ', response:', response);
           res.send(response);
         });
     });
 
     this._router.post('/room', (req, res) => {
+      this._logger.log('ExpressApi: request POST on /room', req.body);
       this._room.addRoom(req.body)
         .then(data => {
           const response = { faults: [], data };
+          this._logger.log('ExpressApi: request POST on /room, response:', response);
           res.send(response);
         })
         .catch(faults => {
           const response = { faults };
+          this._logger.log('ExpressApi: request POST on /room, response:', response);
           res.send(response);
         });
     });
 
     this._router.get('/room', (req, res) => {
+      this._logger.log('ExpressApi: request GET on /room');
       this._room.getAllRooms()
         .then(data => {
           const response = { faults: [], data };
+          this._logger.log('ExpressApi: request GET on /room, response:', response);
           res.send(response);
         })
         .catch(faults => {
           const response = { faults };
+          this._logger.log('ExpressApi: request GET on /room, response:', response);
           res.send(response);
         });
     });
 
     this._router.get('/room/:id/image', (req, res) => {
+      this._logger.log('ExpressApi: request GET on /room/' + req.params.id + '/image');
       this._room.getRoomImage(req.params.id)
         .then(data => {
           const response = { faults: [], data };
+          this._logger.log('ExpressApi: request GET on /room/' + req.params.id + '/image, response:', response);
           res.send(response);
         })
         .catch(faults => {
           const response = { faults };
+          this._logger.log('ExpressApi: request GET on /room/' + req.params.id + '/image, response:', response);
           res.send(response);
         });
     });
